ea3c463205c9ac732c059028dba7c0adec97af34
gip_pro <igor.g.pronkin@gmail.com>
2026-03-18 10:25:33 +0100

    Delete frontend directory

--- frontend/app/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ text }: { text: string }) {
-  return (
-    <button className="px-[28px] py-[14px] rounded-[6px] bg-black text-white hover:opacity-85 transition">
-      {text}
-    </button>
-  );
-}
--- frontend/app/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Container({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-[1160px] mx-auto px-[20px]">
-      {children}
-    </div>
-  );
-}
